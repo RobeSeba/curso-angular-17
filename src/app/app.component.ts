2db2bcff7eb4296e3dacf6340845fe262529254f
Roberto <roberto_m_ll@hotmail.com>
2025-09-02 14:57:44 -0500

    se creo el componente todo en pages y se agregaron decoradores input y output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from "./pages/todo/todo.component";
+import { TODO_DATA } from '../assets/todo';
+import { NTodo } from './models/todo.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ TodoComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-17';
+  tododata1 = TODO_DATA[0];
+  tododata2 = TODO_DATA[1];
+  tododata3 = TODO_DATA[2];
+  tododata4 = TODO_DATA[3];
+  tododata5 = TODO_DATA[4];
+
+  getTodoInfo(event: NTodo.TodoData) {
+    console.log("Evento recibido en el padre", event);
+  }
 }
---
+++ src/app/pages/todo/todo.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NTodo } from '../../models/todo.model';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.scss'
+})
+export class TodoComponent {
+
+  @Input({required: true}) todoData!:NTodo.TodoData ;
+  @Output() onClickIcon = new EventEmitter<NTodo.TodoData>();
+}
